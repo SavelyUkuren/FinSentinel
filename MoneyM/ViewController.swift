bee6f302b135c32492fdc4e74faf41f4589debce
Savely <yoyos9.8split@icloud.com>
2023-08-30 20:14:00 +0300

    Initial Commit

---
+++ MoneyM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyM
+//
+//  Created by Air on 30.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
